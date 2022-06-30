3568c91224c5b3a755a74de16e3f3ee61c771b5e
Sylvan <sylvan700@hotmail.fr>
2022-06-30 14:48:51 +0200

    Project/New-Project-First-Commit

---
+++ SwiftProjectTestGit/SwiftProjectTestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftProjectTestGit
+//
+//  Created by Student06 on 30/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
